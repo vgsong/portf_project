cea85f39d484f86f2972642388e1f621fdd30183
V Song <vgsong@proton.me>
2024-09-20 19:10:07 -0700

    another round

--- classes/reportsender.py
+++ classes/reportsender.py
@@ -24,10 +24,10 @@ class ReportSender:
             0: ['QUIT', exit],
             }
         # index: ['GROUPNAME','FILENAME',[WEEKDAY]]
-        self.contact_mapp = {
-            1: ['A','proj_a_distr.csv',[0,1,2,3,4]],
-            2: ['B','proj_b_distr.csv',[0,2,4]],
-            3: ['C','proj_c_report.csv',[2]],
+        self.send_mapp = {
+            1: ['A','proj_a_distr.csv', [0,1,2,3,4],],
+            2: ['B','proj_b_distr.csv', [0,2,4],],
+            3: ['C','proj_c_distr.csv', [2,4],],
             }  
 
 
@@ -38,18 +38,52 @@ class ReportSender:
             return result
 
     def start_sendreport(self):
-        return
-        # for i, v in self.contact_mapp.items():
-        #     if self.weekday_today in v[2]:
+        for i, v in self.send_mapp.items():
+            to_list = []
+            cc_list = []
 
+            if self.weekday_today in v[2]:
+
+                for x in self.contact_list:
+
+                    if v[0] in x[1] and x[2] == 'TO':
+                        to_list.append(x)
+                    if v[0] in x[1] and x[2] == 'CC':
+                        cc_list.append(x)
+
+                att_dir = os.path.join(self.att_dir, x[1])
+                today_date = self.today_timestamp.strftime('%Y%m%d')
+
+                olapp = Dispatch('Outlook.Application')
+                olmail = olapp.CreateItem(0)
+
+                olmail.To = ';'.join(to_list)
+                olmail.Cc = ';'.join(cc_list)
+                olmail.Subject = 'WEEKLY DISTRIBUTION GROUP {} Rundate {}'.format(v[0], today_date)
+                olmail.Attachments.Add(att_dir) 
+                olmail.HTMLBody = f'Hi -- <br><br>' \
+                                  f'Please see attached for GROUP {v[0]} weekly distribution report <br><br>' \
+                                  f'Let me know if you have any questions'
+                
+                olmail.Display(False)
+                time.sleep(0.5)
+        return
+    
+    def start_menu(self):
+        while True:
+            print('\n----- MAIN MENU ------\n')
+            for i, v in self.menu_mapp.items():
+                print('{}: {}'.format(i, v[0]))
+            user_input = input('Please select menu index number:\n')
+            try:
+                self.menu_mapp[int(user_input)][1]()
+            except Exception as E:
+                print(E)
+                continue
 
 def main():
-    pj = ReportSender()
-    result = []
-    for x in pj.contact_list:
-        if 'A' in x[1] and x[2] == 'TO':
-            result.append(x)
-    print(result)
+    rs = ReportSender()
+    rs.start_menu()
 
 if __name__ == '__main__':
     main()
