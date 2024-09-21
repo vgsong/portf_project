7da2dda1a989692f98004ff809a7e1b4b257728a
V Song <vgsong@proton.me>
2024-09-20 21:49:25 -0700

    keep up

---
+++ classes/emplookup.py
@@ -0,0 +1,61 @@
+from dotenv import load_dotenv
+
+import csv
+import os
+
+
+class EmpLookup:
+    def __init__(self):
+        load_dotenv()
+        self.mdir = os.getenv('MDIR')
+        self.csv_dir = os.path.join(self.mdir, '_csv', self.__class__.__name__.lower())
+        self.csvfiles = os.listdir(os.path.join(self.csv_dir))
+        self.emplist = self.get_emplist()
+
+
+    def get_emplist(self):
+        with open(os.path.join(self.csv_dir, self.csvfiles[0]), 
+                  'r', encoding='utf-8-sig') as cf:
+            data = csv.reader(cf)
+            return [row for row in data]
+
+
+    def start_lookup(self):
+        headers, *details = self.emplist
+        while True:
+            user_input = input('Search by first or last name\n').lower()
+            if user_input == 'q':
+                exit()
+            else:
+                search_result = [row for row in details if user_input in row[1].lower() or user_input in row[2].lower()]
+                break
+        
+        for emp in search_result:
+            empid = str(emp[0])
+            first = str(emp[1])
+            last = str(emp[2])
+            email = str(emp[3])
+            gender = str(emp[4])
+            department = str(emp[5])
+            title = str(emp[6])
+            salary = str(emp[7])
+            hiredate = str(emp[8])
+
+            print('--------------------------')
+            print('{}: {}'.format(headers[0],empid))
+            print('{}: {}'.format(headers[1],first))
+            print('{}: {}'.format(headers[2],last))
+            print('{}: {}'.format(headers[3],email))
+            print('{}: {}'.format(headers[4],gender))
+            print('{}: {}'.format(headers[5],department))
+            print('{}: {}'.format(headers[6],title))
+            print('{}: {}'.format(headers[7],salary))
+            print('{}: {}'.format(headers[8],hiredate))
+            print('--------------------------\n')
+
+def main():
+    a = EmpLookup()
+    a.start_lookup()
+
+if __name__ == '__main__':
+    main()
--- classes/reportsaver.py
+++ classes/reportsaver.py
@@ -13,9 +13,10 @@ class FromDl:
         self.csv_dir = os.path.join(self.mdir, '_csv', self.__class__.__name__.lower())
         self.dailydir = os.path.join(self.csv_dir, self.timestamp)
 
-        self.file_in = ['Project List', 
-                        'AR AGED', 
-                        'GL MAPP', 
+        self.file_in = ['product_list.csv', 
+                        'ar_aged.csv', 
+                        'gl_mapp.csv',
+                        'emp_list.csv',
                         ]
 
     def create_daily_folder(self):
@@ -27,20 +28,23 @@ class FromDl:
         return
 
     def get_dl_files(self):
-        fromdl_dir = os.path.join(self.dailydir)
-        
-
-        print(self.get_latest_report('GL MAPP'))
-            
-
-
+        src_dir = self.dl_dir  # From dir
+        fromdl_dir = os.path.join(self.dailydir)  # To dir
+        for fn in self.file_in:
+            try:
+                os.rename(os.path.join(src_dir, fn),
+                        os.path.join(fromdl_dir, fn),
+                        )
+                print('file {}'.format(fn))
+            except Exception as E:
+                print(E)
+                continue
         return
 
     def get_latest_report(self, report_name):
         _ = [os.path.join(self.dl_dir, x) for x in os.listdir(self.dl_dir) if report_name in x]
         return max(_, key=os.path.getctime)
         
-
 
 def main():
     fd = FromDl()
