0bd8032da61beee2419a23eaaac1b38c1b62dfc1
V Song <vgsong@proton.me>
2024-09-23 09:36:40 -0700

    update main

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
+from dotenv import load_dotenv
+from classes.emplookup import EmpLookup
+from classes.pdfmerger import PdfMerger
 from classes.projlauncher import ProjLauncher
 from classes.reportsaver import FromDl
 from classes.reportsender import ReportSender
 
+import os
+
+class MainLauncher:
+    def __init__(self):
+        load_dotenv()
+
+        self.mdir = os.getenv('MDIR')
+
+        self.menu_mapp = {
+            1: ['EMPLOOKUP', ],
+            2: ['PDFMERGER', ],
+            3: ['PROJLAUNCH', ],
+            4: ['REPORTSAVER', ],
+            5: ['REPORTSENDER', ],
+            0: ['QUIT', ],
+        }
+
+    
+    def start_menu(self):
+        while True:
+            print('------- MAIN MENU -------')
+            for k, v in self.menu_mapp.items():
+                print('{}: {}'.format(k, v[0]))
+            user_input = input('Please select index\n')
+            try:
+                self.menu_mapp[int(user_input)][1]()
+            except Exception as E:
+                print(E)
+
+        
 
 def main():
     print('hello world')
